3118480b3309c13dad9942557dba8a77295a3ad7
abc def <radicaldata@users.noreply.github.com>
2022-03-01 16:17:15 +1000

    update

--- public/lib/php/mail.php
+++ public/lib/php/mail.php
@@ -5,7 +5,7 @@ use Mailgun\Mailgun;
 function contact_mail($name, $phone, $email, $address_line_1, $address_line_2, $city, $state, $country, $postcode) {
 
     $from = 'noreply@mail.web997.com';
-	$to = 'radicalwordpress@gmail.com';
+	$to = 'freebits1@gmail.com';
     $domain = 'mail.web997.com';
     $key = 'key-7a9bb86b317120698eecae4cf3e52143';
 
@@ -19,7 +19,7 @@ function contact_mail($name, $phone, $email, $address_line_1, $address_line_2, $
 		'City: '. $city.PHP_EOL.
 		'State: '. $state.PHP_EOL.
 		'Country: '. $country.PHP_EOL.
-		'Postcode: '. $postcode.PHP_EOL.
+		'Postcode: '. $postcode.PHP_EOL;
 
     $mg = Mailgun::create($key);
     $mg->messages()->send($domain, [
