1a567555f6538e922f51869eb62151e55ebb310c
abc def <radicaldata@users.noreply.github.com>
2022-03-01 16:11:30 +1000

    update

--- public/lib/php/contact_db.php
+++ public/lib/php/contact_db.php
@@ -1,15 +1,18 @@
 <?php
 
-function add_contact(string $name, string $phone, string $email, string $address, string $country, string $postcode) {
-  $dbh = new PDO("pgsql:dbname=techgear.pro", "ubuntu");
+function add_contact(string $name, string $phone, string $email, string $address_line_1, string $address_line_2, string $city, string $state, string $country, string $postcode) {
+  $dbh = new PDO("pgsql:dbname=techgear.pro", "user");
   $sth = $dbh->prepare(
-    'INSERT INTO contact(name, phone, email, address, country, postcode)
-    VALUES (:name, :phone, :email, :address, :country, :postcode)' 
+    'INSERT INTO contact(name, phone, email, address_line_1, :address_line_2, city, state, country, postcode)
+    VALUES (:name, :phone, :email, :address_line_1, :address_line_2, :city, :state, :country, :postcode)' 
   );
   $sth->bindParam(':name', $name, PDO::PARAM_STR);
   $sth->bindParam(':phone', $phone, PDO::PARAM_STR);
   $sth->bindParam(':email', $email, PDO::PARAM_STR);
-  $sth->bindParam(':address', $address, PDO::PARAM_STR);
+  $sth->bindParam(':address_line_1', $address_line_1, PDO::PARAM_STR);
+  $sth->bindParam(':address_line_2', $address_line_2, PDO::PARAM_STR);
+  $sth->bindParam(':city', $city, PDO::PARAM_STR);
+  $sth->bindParam(':state', $state, PDO::PARAM_STR);
   $sth->bindParam(':country', $country, PDO::PARAM_STR);
   $sth->bindParam(':postcode', $postcode, PDO::PARAM_STR);
   $sth->execute();
--- public/lib/php/mail.php
+++ public/lib/php/mail.php
@@ -2,10 +2,10 @@
 require dirname(dirname(dirname((__DIR__)))).'/vendor/autoload.php';
 use Mailgun\Mailgun;
 
-function contact_mail($name, $phone, $email, $address, $country, $postcode) {
+function contact_mail($name, $phone, $email, $address_line_1, $address_line_2, $city, $state, $country, $postcode) {
 
     $from = 'noreply@mail.web997.com';
-	$to = 'freebits1@gmail.com';
+	$to = 'radicalwordpress@gmail.com';
     $domain = 'mail.web997.com';
     $key = 'key-7a9bb86b317120698eecae4cf3e52143';
 
@@ -14,7 +14,10 @@ function contact_mail($name, $phone, $email, $address, $country, $postcode) {
 		'Name: '. $name.PHP_EOL.
 		'Phone: '. $phone.PHP_EOL.
 		'Email: '. $email.PHP_EOL.
-		'Address: '. $address.PHP_EOL.
+		'Address Line 1: '. $address_line_1.PHP_EOL.
+		'Address Line 2: '. $address_line_2.PHP_EOL.
+		'City: '. $city.PHP_EOL.
+		'State: '. $state.PHP_EOL.
 		'Country: '. $country.PHP_EOL.
 		'Postcode: '. $postcode.PHP_EOL.
 
